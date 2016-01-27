3e881cb9e1033dc77b3b306b466ee9b4879fe13e
Yustynn <yustynn@gmail.com>
2016-01-27 12:32:24 -0500

    Fix db

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const path       = require('path');
 
-require('./models');
+require('./db'); // start DB
 
 const app = express();
 
