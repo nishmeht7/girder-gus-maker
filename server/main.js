2205d2cf781ad4e65963bde6c2ec1a5483ad587a
Yustynn <yustynn@gmail.com>
2016-01-27 15:50:44 -0500

    Initial browser setup, change scss to sass

--- server/main.js
+++ server/main.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/*', function (req, res) {
+    res.sendFile(app.get('indexHTMLPath'));
+});
+
 app.use('/api', require('./routes'))
 
 app.listen(1337, () => { console.log('Server eavesdropping on 1337') });
