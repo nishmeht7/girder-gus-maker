2cfc417e3b5b59875359c7ad44f6fb03d5c40946
Brian Dunn <briancollierdunn@gmail.com>
2016-01-27 11:43:31 -0500

    basic setup

--- game/js/states/load.js
+++ game/js/states/load.js
@@ -1,11 +1,17 @@
 var STATE_LOAD = {};
 
 STATE_LOAD.preload = function () {
-
+    game.load.image('BrickBlack', 'game/images/brick_black.png');
+    game.load.image('BrickBreak', 'game/images/brick_break.png');
+    game.load.image('BrickRed', 'game/images/brick_red.png');
+    game.load.image('Girder', 'game/images/girder.png');
+    game.load.image('Tool', 'game/images/tool.png');
+    game.load.spritesheet('Gus', 'game/images/gus.png', 32, 32);
 }
 
 STATE_LOAD.create = function () {
-
-  game.state.start( "game" );
+    console.log('create');
+    game.stage.setBackgroundColor('#000');
+    game.state.start( "game" );
 
 }
