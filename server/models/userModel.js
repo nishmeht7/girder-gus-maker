6107abc4f0fa152a966a0f4904fc56e889053861
everett980 <everett980@gmail.com>
2016-01-27 11:58:19 -0500

    created both models and model index and started a gitignore file

---
+++ server/models/userModel.js
@@ -0,0 +1,13 @@
+'use strict';
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+	name: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
+	following: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+	followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+	likedLevels: [{type: mongoose.Schema.Types.ObjectId, ref: 'Level'}],
+	createdLevels: [{type: mongoose.Schema.Types.ObjectId, ref: 'Level'}]
+});
+
+var User = mongoose.model('User', schema);
