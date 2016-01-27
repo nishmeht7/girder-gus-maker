8b16a4c3603b71ce62465718c725e13b550f6496
everett980 <everett980@gmail.com>
2016-01-27 15:19:41 -0500

    added oauth files

---
+++ server/configure/authentication/authKeys.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'google' = {
+		'clientId' : '398900578513-6ghc0i3n5d85pdcejge40cr6fn2s6qv7.apps.googleusercontent.com',
+		'clientSecret' : 'FF3mXoF96p0KaBMCnOr5D0o6',
+		'callbackUrl' : 'http://127.0.0.1:1337/auth/google/callback'
+	}
+}
