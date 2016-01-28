84495876d9cd20ea7f19c373c662974a873f89f0
Brian Dunn <briancollierdunn@gmail.com>
2016-01-28 16:37:47 -0500

    added hooks and methods to user and level models to manage user totalStars and createdLevels

--- server/db/models/levelModel.js
+++ server/db/models/levelModel.js
@@ -1,5 +1,6 @@
 'use strict';
 const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 const schema = new mongoose.Schema({
     title: {
@@ -24,5 +25,28 @@ const schema = new mongoose.Schema({
     }
 });
 //difficulty was mentioned, but not part of MVP imo
+
+// post-save hook to set total star count of associated user
+schema.post('save', function(doc) {
+    User.findById(doc.creator)
+        .populate('createdLevels', 'starCount')
+        .then(function(user) {
+            return user.setStars();
+        })
+})
+
+// hook to remove deleted level from creator's level list and
+//   set users's new total star count
+schema.post('remove', function(doc) {
+    User.findById(doc.creator)
+        .then(function(user) {
+            // remove level from creator's list
+            return user.removeLevel(doc._id);
+        })
+        // set user's total star count
+        .then(function(user) {
+            return user.setStars();
+        })
+})
 
 mongoose.model('Level', schema);
