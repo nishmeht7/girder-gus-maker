575bfa8b127db2cc9a49a34e0434ec5c4bee93cd
Lancey6 <david.thomas.reeve@gmail.com>
2016-01-27 19:02:20 -0500

    Good enough

--- game/js/consts/index.js
+++ game/js/consts/index.js
@@ -1,2 +1,3 @@
 
-var EPSILON = 0.0001;
+var EPSILON = 0.000001;
+var TAU = Math.PI * 2;
--- game/js/states/boot.js
+++ game/js/states/boot.js
@@ -5,7 +5,8 @@ function initBootState() {
   state.create = function() {
 
     // start game physics
-    game.physics.startSystem( Phaser.Physics.ARCADE );
+    game.physics.startSystem( Phaser.Physics.P2JS );
+    game.physics.p2.setImpactEvents( true );
 
     game.state.start( "load" );
 
