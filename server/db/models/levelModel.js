221a9f7be5ca14d6649764ad4de29e9f5740cb63
Yustynn <yustynn@gmail.com>
2016-01-29 12:55:36 -0500

    Integrate map into Model schema

--- server/db/models/levelModel.js
+++ server/db/models/levelModel.js
@@ -2,78 +2,109 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-const schema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    creator: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    map: {
-        type: String,
-        required: true
-    },
-    dateCreate: {
-        type: Date,
-        default: Date.now()
-    },
-    starCount: {
-        type: Number,
-        default: 0
+// Calculate number of tiles for data validation
+import { TILE_MAP } from '../../misc/constants';
+const numTiles = Object.keys(TILE_MAP).length;
+
+// part of level schema
+const map = {
+  startGirders: {
+    type: Number,
+    default: 0
+  },
+  checksum: {
+    type: String,
+    default: function() {
+      return this.objects.reduce((prev, next) => prev + next, '');
     }
+  },
+  objects: [{
+    tile: {
+      type: Number,
+      max: numTiles
+    },
+    x: {
+      type: Number
+    },
+    y: {
+      type: Number
+    }
+  }],
+  skyColor: {
+    type: String,
+    default: "#4428BC"
+  }
+}
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  map: map,
+  dateCreate: {
+    type: Date,
+    default: Date.now()
+  },
+  starCount: {
+    type: Number,
+    default: 0
+  }
 });
 //difficulty was mentioned, but not part of MVP imo
 
 // note whether level is new before saving
 schema.pre('save', function(next) {
-    this.wasNew = this.isNew;
-    next();
+  this.wasNew = this.isNew;
+  next();
 })
 
 // add level to creator's createdLevels if level is new
 schema.post('save', function(doc, next) {
-    if(doc.wasNew) {
-        User.findById(doc.creator)
-            .then(function(user) {
-                return user.addLevel(doc._id);
-            })
-            .then(function(user) {
-                next();
-            })
-    } else {
+  if (doc.wasNew) {
+    User.findById(doc.creator)
+      .then(function(user) {
+        return user.addLevel(doc._id);
+      })
+      .then(function(user) {
         next();
-    }
+      })
+  } else {
+    next();
+  }
 })
 
 // post-save hook to set total star count of associated user
 schema.post('save', function(doc, next) {
-    User.findById(doc.creator)
-        .populate('createdLevels', 'starCount')
-        .then(function(user) {
-            return user.setStars();
-        })
-        .then(function(user) {
-            next();
-        }).then(null, function(error) {
-			console.error(error);
-			next();
-		});
+  User.findById(doc.creator)
+    .populate('createdLevels', 'starCount')
+    .then(function(user) {
+      return user.setStars();
+    })
+    .then(function(user) {
+      next();
+    }).then(null, function(error) {
+      console.error(error);
+      next();
+    });
 })
 
 // hook to remove deleted level from creator's level list and
 //   set users's new total star count
 schema.post('remove', function(doc) {
-    User.findById(doc.creator)
-        .then(function(user) {
-            // remove level from creator's list
-            return user.removeLevel(doc._id);
-        })
-        // set user's total star count
-        .then(function(user) {
-            return user.setStars();
-        })
+  User.findById(doc.creator)
+    .then(function(user) {
+      // remove level from creator's list
+      return user.removeLevel(doc._id);
+    })
+    // set user's total star count
+    .then(function(user) {
+      return user.setStars();
+    })
 })
 
 mongoose.model('Level', schema);
