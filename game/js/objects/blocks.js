1e4f2cbc537f6982ed0cf2848d535387382e3146
Lancey6 <david.thomas.reeve@gmail.com>
2016-02-03 12:38:35 -0500

    Fixed not being able to put girders where breakable blocks were

--- game/js/objects/blocks.js
+++ game/js/objects/blocks.js
@@ -67,6 +67,7 @@ BreakBrickBlock.prototype = Object.create( Block.prototype );
 
 BreakBrickBlock.prototype.setCollisions = function () {
 
+  this.sprite.body.setRectangle( 32, 32 );
   this.sprite.body.setCollisionGroup( COLLISION_GROUPS.BLOCK_ROTATE );
   this.sprite.body.collides( [ COLLISION_GROUPS.PLAYER_SOLID, COLLISION_GROUPS.PLAYER_SENSOR ] );
   this.sprite.body.onBeginContact.add( BreakBrickBlock.prototype.startCollapsing, this );
@@ -98,7 +99,7 @@ BreakBrickBlock.prototype.collapse = function () {
   if ( !this.sprite.visible ) return;
 
   this.sprite.visible = false;
-  this.sprite.body.clearCollision();
+  this.sprite.body.clearShapes();
 
   // make some particles!
   new ParticleBurst( this.sprite.position.x, this.sprite.position.y, "Debris", {
