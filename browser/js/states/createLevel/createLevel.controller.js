fab8d1258390d0b19deb4a1a21c95900c136ad78
everett980 <everett980@gmail.com>
2016-02-03 14:23:45 -0500

    added button to switch between level editor and playing game. playing game now requires the controller of the page to have an event emitter to specify the mongo id of the level to play

--- browser/js/states/createLevel/createLevel.controller.js
+++ browser/js/states/createLevel/createLevel.controller.js
@@ -2,55 +2,67 @@ const _ = require('lodash');
 const eventEmitter = window.eventEmitter
 
 app.controller('CreateLevelCtrl', function($scope) {
+	$scope.testing = false;
+
 	$scope.toolArr = {
-    'Eraser' : {
-      img : '/assets/images/eraser.png',
-      tile: null
-    },
-    'Gus' : {
-			img : '/assets/images/gus-static.png',
-      tile: 'Gus'
+		'Eraser' : {
+			img : '/assets/images/eraser.png',
+	tile: null
 		},
-    'Red Brick' : {
-			img : '/assets/images/brick_red.png',
-      tile: 'RedBrickBlock'
-		},
-		'Black Brick' : {
-			img : '/assets/images/brick_black.png',
-      tile: 'BlackBrickBlock'
-		},
-		'Break Brick' : {
-			img : '/assets/images/brick_break.png',
-      tile: 'BreakBrickBlock'
-		},
-		'Tool' : {
-			img : '/assets/images/tool.png',
-      tile: 'Tool'
-		}
+	'Gus' : {
+		img : '/assets/images/gus-static.png',
+	tile: 'Gus'
+	},
+	'Red Brick' : {
+		img : '/assets/images/brick_red.png',
+	tile: 'RedBrickBlock'
+	},
+	'Black Brick' : {
+		img : '/assets/images/brick_black.png',
+	tile: 'BlackBrickBlock'
+	},
+	'Break Brick' : {
+		img : '/assets/images/brick_break.png',
+tile: 'BreakBrickBlock'
+	},
+	'Tool' : {
+		img : '/assets/images/tool.png',
+		tile: 'Tool'
+	}
 	}
 
-  $scope.changeActiveTool = function(tool) {
-    console.log('changing active tool...')
-    eventEmitter.emit('change active tool', tool.tile)
-    $scope.activeToolImg = tool.img;
-  }
+	$scope.changeActiveTool = function(tool) {
+		console.log('changing active tool...')
+			eventEmitter.emit('change active tool', tool.tile)
+			$scope.activeToolImg = tool.img;
+	}
 
-  $scope.requestParsedTileMap = () => {
-    console.log('requesting tile map...')
-    eventEmitter.emit('request tile map', '');
-  }
+	$scope.requestParsedTileMap = () => {
+		console.log('requesting tile map...')
+			eventEmitter.emit('request tile map', '');
+	}
 
-  eventEmitter.on('send tile map', (parsedTileMap) => {
-    console.log('recieved.')
-    console.dir(parsedTileMap);
-  });
+	eventEmitter.on('send tile map', (parsedTileMap) => {
+		console.log('recieved.')
+		console.dir(parsedTileMap);
+	});
 
-  $scope.getScreenshot = function() {
-    eventEmitter.emit('request screenshot');
-  }
+	$scope.getScreenshot = function() {
+		eventEmitter.emit('request screenshot');
+	}
 
-  eventEmitter.on('send screenshot', (screenshot) => {
-    console.log('screenshot');
-    console.log(screenshot);
-  })
+	$scope.testTesting = function() {
+		window.game = null;
+		$scope.testing = !$scope.testing;
+	}
+
+	eventEmitter.on('send screenshot', (screenshot) => {
+		console.log('screenshot');
+		console.log(screenshot);
+	})
+
+	eventEmitter.on('what level to play', (data) => {
+		console.log(data);
+		eventEmitter.emit('play this level', 'testing');
+	});
 });
