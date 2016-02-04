4394e4f768013e21287a0fa23a8faadc9e4910df
everett980 <everett980@gmail.com>
2016-02-04 17:05:21 -0500

    can save file

--- server/routes/levels.js
+++ server/routes/levels.js
@@ -32,5 +32,8 @@ router.get('/:id/map', getDocAndSend('Level', ['map'], ['map']));
 // user can update own level
 router.put('/:id', mustBeLoggedIn, getDocAndUpdateIfOwnerOrAdmin('Level'));
 
+// user can create own level
+router.post('/', mustBeLoggedIn, (req, res, next) => {console.log(req, res, next); next()}, createDoc('Level', 'creator'));
+
 // user can delete own level
 router.delete('/:id', mustBeLoggedIn, getDocAndDeleteIfOwnerOrAdmin('Level'));
