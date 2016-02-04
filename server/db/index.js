cd769de1e4c41ac1ba77ae3db97b07d41bb7a971
Lancey6 <david.thomas.reeve@gmail.com>
2016-02-04 14:29:11 -0500

    Fixed database erroring out on connect

--- server/db/index.js
+++ server/db/index.js
@@ -4,17 +4,14 @@ const path = require('path');
 const chalk = require('chalk');
 
 var mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://gus:gusIsGreat@ds051655.mongolab.com:51655/ggmng').connection;
+const db = mongoose.connect('mongodb://gus:gusIsGreat@ds051655.mongolab.com:51655/ggmng');
 
 // Require our models -- these should register the model into mongoose
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
 require('./models');
 
-const startDbPromise = new Promise(function (resolve, reject) {
-    db.on('open', resolve);
-    db.on('error', reject);
-});
+const startDbPromise = db;
 
 console.log(chalk.yellow('Opening connection to MongoDB . . .'));
 startDbPromise.then(function () {
