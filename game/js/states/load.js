1bf397be09e4470745cde1c62659ed653eaa18d3
everett980 <everett980@gmail.com>
2016-02-04 16:11:30 -0500

    added fields to specify girder count and sky color, bug: if color is 000000 girder gus treats as false and defaults to blue, if the number of girders specified is 0, girder gus defaults to 10 girders. input type=number does not display the number inside of it unless highlighted

--- game/js/states/load.js
+++ game/js/states/load.js
@@ -83,13 +83,11 @@ function initLoadState() {
     eventEmitter.on('play this level', function(data) {
       if(data[0] === 'levelArr') {
         loadText.text = "Creating level...";
-
-        game.level = {
-          skyColor: '#FFBB22',
-          startGirders: 12,
-          objects: data[1]
-        };
-
+				game.level = {
+					sky: data[1].skyColor,
+					girders: data[1].girdersAllowed,
+					objs: data[1].levelArr
+				};
         state.gotoStart();
       } else if (data[0] === 'levelId') {
         loadText.text = "Downloading level...";
