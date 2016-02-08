4f6e35185b855c8600f7201c24652f8e53a258fd
Yustynn <yustynn@gmail.com>
2016-02-08 12:16:52 -0500

    Remove ghost dev <pre></pre> in index.html

--- game/js/objects/recordingGus.js
+++ game/js/objects/recordingGus.js
@@ -85,7 +85,7 @@ class RecordingGus extends Gus {
   kill() {
     this.inputRecords = this.inputRecords.reverse();
 
-    document.getElementById('arr').textContent = JSON.stringify(this.inputRecords);
+    // document.getElementById('arr').textContent = JSON.stringify(this.inputRecords);
     // document.getElementById('arr').textContent = this.records;
 
     new ParticleBurst( this.sprite.position.x, this.sprite.position.y, "GusHead", {
