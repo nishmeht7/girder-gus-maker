3eeb64d58cbb9208994cb89e674303d1ff07c3de
Lancey6 <david.thomas.reeve@gmail.com>
2016-02-10 15:07:01 -0500

    Fixed zombie mode when skipping to the next level outside of a playlist (that's also a bug)

--- game/js/states/game.js
+++ game/js/states/game.js
@@ -263,6 +263,8 @@ function initGameState() {
     game.camera.displayObject.position = game.dolly.position;
     game.camera.displayObject.rotation = game.dolly.rotation;
 
+    this.resultScreen = undefined;
+
     //game.world.destroy();
     game.state.clearCurrentState();
     game.stage.setBackgroundColor( "#000" );
@@ -272,8 +274,6 @@ function initGameState() {
   }
 
   state.postBroadphase = function ( body1, body2 ) {
-
-    if ( state.resultScreen ) return true;
 
     if ( body1.sprite.name === "Gus" && body2.sprite.name === "Tool" && body1.fixedRotation && gus.isDead === false && body1.gameObject.constructor.name !== 'GhostGus' ) {
       body2.sprite.owner.collect();
