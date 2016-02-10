97b6a27b029e13a24ac554d4a0cdc539dc6f557f
Brian Dunn <briancollierdunn@gmail.com>
2016-02-09 19:54:17 -0500

    optimistic follow and star buttons

--- browser/js/directives/user-minicard/user-minicard.controller.js
+++ browser/js/directives/user-minicard/user-minicard.controller.js
@@ -6,27 +6,59 @@ app.controller('UserMinicardCtrl', function($scope, SocialFactory) {
     })();
     $scope.pending = false;
 
-    $scope.followCreator = function() {
-        if($scope.user !== null) {
-            $scope.pending = true;
-            SocialFactory.userFollower($scope.creator._id, 'followUser')
-                .then(function(res) {
+    var optimistic = false;
+    var optimisticCache;
+    var optimisticTimer;
+
+    var serverFollowToggle = function(action) {
+        SocialFactory.userFollower($scope.creator._id, action)
+            .then(function(res) {
+                if(optimisticCache !== undefined && action !== optimisticCache) {
+                    var cacheAction = optimisticCache;
+                    optimisticCache = undefined;
+                    serverFollowToggle(cacheAction);
+                } else {
+                    clearTimeout(optimisticTimer);
+                    optimistic = false;
+                    optimisticCache = undefined;
                     $scope.creator.totalFollowers = res.creator.totalFollowers;
                     $scope.followed = res.user.following.indexOf($scope.creator._id) !== -1;
                     $scope.pending = false;
-                })
+                }
+            });
+    }
+
+    $scope.followCreator = function() {
+        $scope.pending = true;
+        optimisticTimer = setTimeout(function() {
+            $scope.followed = true;
+            $scope.creator.totalFollowers++;
+            $scope.pending = false;
+            optimistic = true;
+            $scope.$digest();
+        }, 200);
+
+        if(optimistic) {
+            optimisticCache = 'followUser';
+        } else {
+            serverFollowToggle('followUser');
         }
     }
 
     $scope.unfollowCreator = function() {
-        if($scope.user !== null) {
-            $scope.pending = true;
-            SocialFactory.userFollower($scope.creator._id, 'unfollowUser')
-                .then(function(res) {
-                    $scope.creator.totalFollowers = res.creator.totalFollowers;
-                    $scope.followed = res.user.following.indexOf($scope.creator._id) !== -1;
-                    $scope.pending = false;
-                })
+        $scope.pending = true;
+        optimisticTimer = setTimeout(function() {
+            $scope.followed = false;
+            $scope.creator.totalFollowers--;
+            $scope.pending = false;
+            optimistic = true;
+            $scope.$digest();
+        }, 200);
+
+        if(optimistic) {
+            optimisticCache = 'unfollowUser';
+        } else {
+            serverFollowToggle('unfollowUser');
         }
     }
 });
