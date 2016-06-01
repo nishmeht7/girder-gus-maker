210b3a82f832334b559603264eafba5976cd4e37
everett980 <everett980@gmail.com>
2016-05-31 20:18:25 -0400

    adds warning to game pages advising refresh if no girders

--- browser/js/directives/index.js
+++ browser/js/directives/index.js
@@ -7,3 +7,4 @@ require('./level-creator');
 require('./user-minicard')
 require('./leaderboard');
 require('./share');
+require('./reload-warning');
---
+++ browser/js/directives/reload-warning/reloadWarning.js
@@ -0,0 +1,7 @@
+app.directive('reloadWarning', () => {
+	return {
+		restrict: 'E',
+		scope: true,
+		templateUrl: 'js/directives/reload-warning/reload-warning.html'
+	}
+});
